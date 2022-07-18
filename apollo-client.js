203e36c2fdfc824c9601d9400b17e275435bedcc
Nico Ignacio <norbertoignaciojr@gmail.com>
2022-07-18 16:16:01 +0800

    main: graphql and network connection

--- apollo-client.js
+++ apollo-client.js
@@ -5,7 +5,7 @@ import { ApolloClient, InMemoryCache } from "@apollo/client";
 const client = new ApolloClient({
     uri: "https://zion.stepzen.net/api/messy-lion/__graphql ",
     headers: {
-        Authorization: `Apikey ${process.env.STEPZEN_API_KEY}`,
+        Authorization: `Apikey ${process.env.NEXT_PUBLIC_STEPZEN_KEY}`,
     },
     cache: new InMemoryCache(),
 });
