e1cad5b625f2a006b5a04ae762663612ab9bee40
Nico Ignacio <norbertoignaciojr@gmail.com>
2022-07-24 17:03:58 +0800

    main: added comment

--- graphql/mutations.ts
+++ graphql/mutations.ts
@@ -35,3 +35,15 @@ export const ADD_SUBREDDIT = gql`
         }
     }
 `;
+
+export const ADD_COMMENT = gql`
+    mutation MyMutation($post_id: ID!, $username: String!, $text: String!) {
+        insertComment(post_id: $post_id, username: $username, text: $text) {
+            created_at
+            id
+            post_id
+            text
+            username
+        }
+    }
+`;
--- pages/post/[postId].tsx
+++ pages/post/[postId].tsx
@@ -1,13 +1,25 @@
-import { useQuery } from "@apollo/client";
+import { useMutation, useQuery } from "@apollo/client";
 import { useSession } from "next-auth/react";
 import { useRouter } from "next/router";
 import React from "react";
+import { SubmitHandler, useForm } from "react-hook-form";
+import toast from "react-hot-toast";
+import TimeAgo from "react-timeago";
+import Avatar from "../../components/Avatar";
 import Post from "../../components/Post";
+import { ADD_COMMENT } from "../../graphql/mutations";
 import { GET_POST_BY_POST_ID } from "../../graphql/queries";
+
+type FormData = {
+    comment: string;
+};
 
 function PostPage() {
     const { query } = useRouter();
     const { postId } = query;
+    const [addComment] = useMutation(ADD_COMMENT, {
+        refetchQueries: [GET_POST_BY_POST_ID, "getPostByPostId"],
+    });
     const { data } = useQuery(GET_POST_BY_POST_ID, {
         variables: {
             post_id: postId,
@@ -17,6 +29,30 @@ function PostPage() {
     const { data: session } = useSession();
 
     const post: Post = data?.getPostByPostId;
+    const {
+        register,
+        handleSubmit,
+        watch,
+        setValue,
+        formState: { errors },
+    } = useForm<FormData>();
+
+    const onSubmit: SubmitHandler<FormData> = async (data) => {
+        console.log(data);
+        const notification = toast.loading("Posting your comment...");
+
+        await addComment({
+            variables: {
+                post_id: postId,
+                username: session?.user?.name,
+                text: data.comment,
+            },
+        });
+
+        setValue("comment", "");
+        toast.success("Comment Posted!", { id: notification });
+    };
+    console.log(data);
 
     return (
         <div className="mx-auto max-w-5xl my-7">
@@ -27,8 +63,12 @@ function PostPage() {
                     Comment as <span>{session?.user?.name}</span>
                 </p>
 
-                <form>
+                <form
+                    className="flex flex-col space-y-2"
+                    onSubmit={handleSubmit(onSubmit)}
+                >
                     <textarea
+                        {...register("comment")}
                         className="h-24 rounded-md border border-gray-300 p-2 ml-4 outline-none disabled:bg-gray-50"
                         placeholder={
                             session
@@ -36,7 +76,7 @@ function PostPage() {
                                 : "Sign in to comment"
                         }
                         disabled={!session}
-                    ></textarea>
+                    />
 
                     <button
                         type="submit"
@@ -45,6 +85,33 @@ function PostPage() {
                         Comment
                     </button>
                 </form>
+
+                <div className="-my-5 rounded-b-md border border-t-0 border-gray-300 bg-white py-5 px-10">
+                    <hr className="py-2" />
+                    {post?.comments.map((comment) => (
+                        <div
+                            className="relative flex items-center space-x-2 space-y-5"
+                            key={comment.id}
+                        >
+                            <hr className="absolute top-10 h-16 border left-7 z-0" />
+                            <div className="z-50">
+                                <Avatar seed={comment.username} />
+                            </div>
+                            <div className="flex flex-col">
+                                <p>
+                                    <span className="font-semibold text-gray-600">
+                                        {comment.username}
+                                    </span>{" "}
+                                    <TimeAgo
+                                        date={comment.created_at}
+                                        className="text-sm text-gray-400"
+                                    />
+                                </p>
+                                <p>{comment.text}</p>
+                            </div>
+                        </div>
+                    ))}
+                </div>
             </div>
         </div>
     );
